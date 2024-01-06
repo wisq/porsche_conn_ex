379e0bbd30ff9f353e487c7d00d13405b24986c6
Adrian Irving-Beer <wisq@wisq.net>
2024-01-06 18:25:11 -0500

    `mix new porsche_conn_ex`

---
+++ lib/porsche_conn_ex.ex
@@ -0,0 +1,18 @@
+defmodule PorscheConnEx do
+  @moduledoc """
+  Documentation for `PorscheConnEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PorscheConnEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/porsche_conn_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule PorscheConnExTest do
+  use ExUnit.Case
+  doctest PorscheConnEx
+
+  test "greets the world" do
+    assert PorscheConnEx.hello() == :world
+  end
+end
