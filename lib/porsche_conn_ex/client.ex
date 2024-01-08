3e40e7585497050e94c73a407ac28edc32e35df9
Adrian Irving-Beer <wisq@wisq.net>
2024-01-08 18:48:01 -0500

    More client API.

--- lib/porsche_conn_ex/client.ex
+++ lib/porsche_conn_ex/client.ex
@@ -4,68 +4,89 @@ defmodule PorscheConnEx.Client do
   alias PorscheConnEx.Session
   alias PorscheConnEx.Config
 
-  @api_server "api.porsche.com"
-
-  @model_error "Failed to convert value of type 'java.lang.String' to required type 'de.porsche.myservices.emobility.dtos.vcs.CarModel'"
-  @vin_error "GRAY_SLICE_ERROR_UNKNOWN_MSG"
+  @wait_secs 120
 
   def vehicles(session, config \\ %Config{}) do
-    Req.new(
-      url: "https://#{@api_server}/core/api/v3/#{Config.url(config)}/vehicles",
-      headers: Session.headers(session)
+    get(session, "/core/api/v3/#{Config.url(config)}/vehicles")
+  end
+
+  def summary(session, vin) do
+    get(session, "/service-vehicle/vehicle-summary/#{vin}")
+  end
+
+  def stored_overview(session, vin, config \\ %Config{}) do
+    get(session, "/service-vehicle/#{Config.url(config)}/vehicle-data/#{vin}/stored")
+  end
+
+  def current_overview(session, vin, config \\ %Config{}) do
+    url = "/service-vehicle/#{Config.url(config)}/vehicle-data/#{vin}/current/request"
+
+    post_and_wait(
+      session,
+      url,
+      fn req_id -> "#{url}/#{req_id}/status" end,
+      fn req_id -> "#{url}/#{req_id}" end,
+      # avoids "missing content-length" error
+      body: ""
     )
-    |> Req.get()
-    |> handle()
   end
 
   def capabilities(session, vin) do
-    Req.new(
-      url: "https://#{@api_server}/service-vehicle/vcs/capabilities/#{vin}",
-      headers: Session.headers(session)
-    )
-    |> with_handle_unknown_vin()
-    |> Req.get()
-    |> handle()
+    get(session, "/service-vehicle/vcs/capabilities/#{vin}")
+  end
+
+  def maintenance(session, vin) do
+    get(session, "/predictive-maintenance/information/#{vin}")
   end
 
   def emobility(session, vin, model, config \\ %Config{}) do
-    Req.new(
-      url: "https://#{@api_server}/e-mobility/#{Config.url(config)}/#{model}/#{vin}",
-      params: %{timezone: config.timezone},
-      headers: Session.headers(session)
+    get(
+      session,
+      "/e-mobility/#{Config.url(config)}/#{model}/#{vin}",
+      params: %{timezone: config.timezone}
     )
-    |> with_handle_unknown_vin()
+  end
+
+  defp get(session, url, opts \\ []) do
+    req_new(session, url, opts)
     |> Req.get()
     |> handle()
   end
 
+  defp post(session, url, opts) do
+    req_new(session, url, opts)
+    |> Req.post()
+    |> handle()
+  end
+
+  defp post_and_wait(session, url, wait_url_fn, final_url_fn, opts) do
+    case post(session, url, opts) do
+      {:ok, %{"requestId" => req_id}} ->
+        1..@wait_secs
+        |> Enum.reduce_while(nil, fn _, _ ->
+          continue =
+            case get(session, wait_url_fn.(req_id)) do
+              {:ok, %{"status" => status}} -> status == "IN_PROGRESS"
+              {:ok, %{"actionState" => status}} -> status == "IN_PROGRESS"
+            end
+
+          {if(continue, do: :cont, else: :halt), nil}
+        end)
+
+        get(session, final_url_fn.(req_id))
+    end
+  end
+
+  defp req_new(session, url, opts) do
+    headers = Session.headers(session)
+
+    opts
+    |> Keyword.put(:url, "https://api.porsche.com/#{url}")
+    |> Keyword.update(:headers, headers, &Map.merge(&1, headers))
+    |> Req.new()
+  end
+
   defp handle({:ok, %{status: 200, body: %{} = body}}), do: {:ok, body}
-
-  defp handle({:ok, %{status: 404, body: %{"pcckErrorKey" => @vin_error}}}),
-    do: {:error, :unknown_vin}
-
-  defp handle({:ok, %{status: 400, body: %{"pcckErrorMessage" => @model_error <> _}}}),
-    do: {:error, :unknown_model}
-
   defp handle({:ok, resp}), do: {:error, resp}
   defp handle({:error, err}), do: {:error, err}
-
-  # Unknown VINs normally result in a 502 status, which Req thinks is a transient error.
-  # This changes that to a 404 status and prevents retries.
-  defp with_handle_unknown_vin(request) do
-    request
-    |> Req.Request.prepend_response_steps(
-      handle_unknown_vin: fn
-        {request, %{status: 502, body: body} = response} ->
-          if String.contains?(body, @vin_error) do
-            {request, %{response | status: 404}}
-          else
-            {request, response}
-          end
-
-        {request, response} ->
-          {request, response}
-      end
-    )
-  end
 end
